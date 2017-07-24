a0f8abf6ba5e81b4cf212eaf638dd62314e630f9
Rob Bishop <GreatBigBore@gmail.com>
2017-07-24 08:08:05 -0700

    Initial Commit

---
+++ Archonia_Swift/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Archonia_Swift
+//
+//  Created by Rob Bishop on 7/24/17.
+//  Copyright © 2017 Rob Bishop. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
