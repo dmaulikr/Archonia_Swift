e28846fc122229b301434ea21becdf59e8243ed9
Rob Bishop <GreatBigBore@gmail.com>
2017-07-25 09:49:41 -0700

    Beginnings of Forager

---
+++ Archonia_Swift/Phenotype/Forager.swift
@@ -0,0 +1,13 @@
+//
+//  Forager.swift
+//  Archonia_Swift
+//
+//  Created by Rob Bishop on 7/25/17.
+//  Copyright © 2017 Rob Bishop. All rights reserved.
+//
+
+import Foundation
+
+struct Forager {
+    
+}
